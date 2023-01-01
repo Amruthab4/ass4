640b003714a7d475993ea145272f88b027538bb1
Amruthab4 <89182134+Amruthab4@users.noreply.github.com>
2023-01-01 23:07:01 +0530

    first commit

---
+++ ass 4/query3 (1).sql
@@ -0,0 +1,7 @@
+INSERT INTO FootballVenue(venue_id int,venue_name varchar(100),city_id int,capacity int)
+
+VALUES (1001,'devils',01,10000),(1002,'RoyalchallengersBanglore',02,20000),
+(1003,'ChennaiSuperkings',03,30000),(1004,'KolkataKnightriders',04,40000),
+(1005,'DelhiKnights',05,5000),(1006,'HaryanaSteelers',06,60000),
+(1007,'Delhidaredevils',07,70000),(1008,'Pradesh',08,80000),
+(1009,'KeralaWarriors',09,90000),(1100,'Goakings',10,100000);
